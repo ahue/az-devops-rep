4c2e30645baae4783d11516403cf6f1ba45769ea
Andreas Huebner <ahue87@gmail.com>
2022-02-06 11:01:58 +0100

    initial commit

---
+++ azure-pull-requests.py
@@ -0,0 +1,40 @@
+from azure.devops.connection import Connection
+from msrest.authentication import BasicAuthentication
+from azure.devops.released.git import GitPullRequestSearchCriteria
+import yaml
+import json
+import sys
+
+def get_prs(config_file: str, project: str, outfile: str):
+    with open(config_file) as file:
+        try:
+            cfg = yaml.safe_load(file)  
+            print(cfg)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    pat = cfg["pat"]
+    organization_url = f'https://dev.azure.com/{cfg["org"]}'
+
+    # Create a connection to the org
+    credentials = BasicAuthentication('', pat)
+    connection = Connection(base_url=organization_url, creds=credentials)
+
+    client = connection.clients.get_git_client()
+
+    sc = GitPullRequestSearchCriteria()
+    sc.status = "all"
+
+    prs = client.get_pull_requests_by_project(project=project, search_criteria=sc)
+    prl = [pr.as_dict() for pr in prs]
+
+    with open(outfile, "w") as outfile:
+        outfile.write(json.dumps(prl))
+
+if __name__ == "__main__":
+    print(sys.argv)
+    if len(sys.argv) != 4:
+        print("Expecting arguments config_file, project and output file")
+        raise ValueError()
+    else:
+        get_prs(sys.argv[1], sys.argv[2], sys.argv[3])
